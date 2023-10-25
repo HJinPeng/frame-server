cca4f32d766bf9ba415c32d0295eca757cdf2a5a
jinpengh <742463015@qq.com>
2023-10-26 00:37:59 +0800

    feat: 添加中间件，更新信息和创建信息

---
+++ src/middleware/operation-info.js
@@ -0,0 +1,17 @@
+const dayjs = require('dayjs')
+
+module.exports = function () {
+  return async function ( ctx, next ) {
+    ctx.state.updateInfo = {
+      updateBy: ctx.state.user?.account,
+      updateByName: ctx.state.user?.realname,
+      updateDateTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
+    }
+    ctx.state.createInfo = {
+      createBy: ctx.state.user?.account,
+      createByName: ctx.state.user?.realname,
+      createDateTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
+    }
+    await next()
+  }
+}
